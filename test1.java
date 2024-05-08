1fdcfc0d51e15bf14488439b7df87bb6eb8d363b
Subbuvnc <subbuvnc@gmail.com>
2024-05-08 22:27:10 +0530

    dev 2 test1.java modification

--- test1.java
+++ test1.java
@@ -1,5 +1,9 @@
 public class test1{
+    public void m1(){
+     System.out.println("modified by developer 2");
+}
     public static void main(String[] args){
        System.out.println("hello");
+       
 }
 }
